78fe0eb94f2b3c60ec0788ff0b1cc8782bad7e8d
Alen D. Osdaña <111480460+lnsdn1@users.noreply.github.com>
2022-09-05 10:32:37 +0800

    Create Character.java

---
+++ Character.java
@@ -0,0 +1,37 @@
+public class Character {
+    // Character attributes
+    String fullname;
+    String surname;
+    String firstname;
+    String middleinitial;
+    int dateofbirth;
+    int studentnumber;
+    String studentemailadress;
+    boolean amiawesome;
+    public String studentemailaddress;
+    // Character method
+    public void sayMysurname() {
+        System.out.println("My surname is " + surname);
+    }
+    public void sayMyStudentNumber() {
+        System.out.println("My Student Number is " + studentnumber);
+    }
+    public void sayMyEmailAddress() {
+        System.out.println("My Email Address is " + studentemailaddress);
+    }
+    public void sayamIawesome() {
+        System.out.println("I AM SUPERRRRRR AWESOMEEEEEEE ");
+    }
+    public void sayMyfirstname() {
+        System.out.println("My first name is " + firstname);
+    }
+    public void saymiddleinitial() {
+        System.out.println("My middle initial is " + middleinitial);
+    }
+    public void sayMyfullname() {
+        System.out.println("My fullname is " + fullname);
+    }
+    public void sayMydateofbirth() {
+        System.out.println("My date of Birth is" + dateofbirth);
+    }
+}
